fe662e4fdf70ae8958c3e0a540ebb8dbf4b11609
samuelcht <schtl2021@gmail.com>
2024-09-19 09:20:23 +0800

    Create app.js

---
+++ Kubernetes/app.js
@@ -0,0 +1,8 @@
+var http = require('http');
+var ip = require('ip');
+function onRequest(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello I am ' + ip.address() + '!');
+  console.log('Incoming connection from ' + req.connection.remoteAddress);
+}
+var server = http.createServer(onRequest).listen(process.env.PORT || 1234);
